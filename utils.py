ef7b51d3624b339757779c59c9a893f9dd428276
Shirsha07 <shirsha7477@gmail.com>
2025-04-19 19:38:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,43 +10,57 @@ def fetch_stock_data(ticker, start_date, end_date):
 
 def calculate_indicators(data):
     close = data["Close"]
-    
+
     # MACD
-    macd_raw = MACD(close).macd()
-    data["MACD"] = pd.Series(macd_raw.values.ravel(), index=data.index)
+    macd = MACD(close).macd()
+    data["MACD"] = pd.Series(macd.values.ravel(), index=data.index)
 
     # RSI
-    rsi_raw = RSIIndicator(close).rsi()
-    data["RSI"] = pd.Series(rsi_raw.values.ravel(), index=data.index)
+    rsi = RSIIndicator(close).rsi()
+    data["RSI"] = pd.Series(rsi.values.ravel(), index=data.index)
 
-    # EMA
-    ema_raw = EMAIndicator(close, window=20).ema_indicator()
-    data["EMA20"] = pd.Series(ema_raw.values.ravel(), index=data.index)
+    # EMA 20
+    ema = EMAIndicator(close, window=20).ema_indicator()
+    data["EMA20"] = pd.Series(ema.values.ravel(), index=data.index)
 
     # Bollinger Bands
     bb = BollingerBands(close)
-    upper_band = bb.bollinger_hband()
-    lower_band = bb.bollinger_lband()
-    data["BB_upper"] = pd.Series(upper_band.values.ravel(), index=data.index)
-    data["BB_lower"] = pd.Series(lower_band.values.ravel(), index=data.index)
+    upper = bb.bollinger_hband()
+    lower = bb.bollinger_lband()
+    data["BB_upper"] = pd.Series(upper.values.ravel(), index=data.index)
+    data["BB_lower"] = pd.Series(lower.values.ravel(), index=data.index)
 
     return data
 
 def filter_upward_trending_stocks(data):
+    # Align all operands by reindexing to data.index just in case
+    close = data["Close"].reindex(data.index)
+    macd = data["MACD"].reindex(data.index)
+    rsi = data["RSI"].reindex(data.index)
+    bb_upper = data["BB_upper"].reindex(data.index)
+    ema20 = data["EMA20"].reindex(data.index)
+
     return data[
-        (data["MACD"] > 0) &
-        (data["RSI"] > 50) &
-        (data["Close"] >= data["BB_upper"]) &
-        (data["Close"] > data["EMA20"])
+        (macd > 0) &
+        (rsi > 50) &
+        (close >= bb_upper) &
+        (close > ema20)
     ]
 
 def filter_downward_trending_stocks(data):
+    close = data["Close"].reindex(data.index)
+    macd = data["MACD"].reindex(data.index)
+    rsi = data["RSI"].reindex(data.index)
+    bb_lower = data["BB_lower"].reindex(data.index)
+    ema20 = data["EMA20"].reindex(data.index)
+
     return data[
-        (data["MACD"] < 0) &
-        (data["RSI"] < 50) &
-        (data["Close"] <= data["BB_lower"]) &
-        (data["Close"] < data["EMA20"])
+        (macd < 0) &
+        (rsi < 50) &
+        (close <= bb_lower) &
+        (close < ema20)
     ]
+
 
 
 
